6ec150c9f00e9084cf4449f010de2b64c3e8d777
Chris <christopher.roberts-10@student.manchester.ac.uk>
2024-07-07 13:21:11 +0100

    feat: implement config file

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+"""Config settings for the backend."""
+
+
+class Settings:  # pylint: disable=too-few-public-methods
+    """Config settings for the backend."""
+
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./backend.db"
