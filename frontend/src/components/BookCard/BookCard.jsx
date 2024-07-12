2e7d5aed3ba0285b0cc7a3f877b70e38a7d97af2
Chris <christopher.roberts-10@student.manchester.ac.uk>
2024-07-12 20:53:53 +0100

    fix: change book card stylings

--- frontend/src/components/BookCard/BookCard.jsx
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -4,8 +4,8 @@ import {Card, CardHeader, CardBody, Divider} from "@nextui-org/react";
 export const BookCard = ({ book }) => {
     return (
         // Probably want to add a link to listings page here at some point
-        <Card className="max-w-[270px]">
-            <CardHeader className="flex gap-3">
+        <Card className="max-w-[400px] min-w-[400px]">
+            <CardHeader className="flex gap-10">
                 <div className="flex flex-col">
                     <p className="text-md">{book.title}</p>
                     <p className="text-small text-default-500">By {book.author}</p>
